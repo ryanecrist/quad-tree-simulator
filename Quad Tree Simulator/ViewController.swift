1ba9d87c8d75defe4a72775404eb5c88901b4cd5
Ryan Crist <ryan.e.crist@gmail.com>
2018-11-30 00:12:12 -0500

    Initial Commit

---
+++ Quad Tree Simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quad Tree Simulator
+//
+//  Created by Ryan Crist on 11/30/18.
+//  Copyright © 2018 Ryan Crist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
